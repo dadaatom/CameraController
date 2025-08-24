ab7f6f9a90e2248123097ed5cdc081e83e8a2e84
dadaatom <11johnson1111@gmail.com>
2025-08-24 15:09:08 -0400

    New Camera Tracking Modes

--- CameraController.cs
+++ CameraController.cs
@@ -1,4 +1,6 @@
-﻿namespace CameraControl
+﻿using UnityEngine.Serialization;
+
+namespace CameraControl
 {
 	using UnityEngine;
 
@@ -10,12 +12,21 @@
 		public CameraZoomer CameraZoomer { get; set; }
 		
 		public Vector2 cameraOffset;
-		public float cameraSpeed;
+
+		public CameraTrackingType cameraTrackingType;
+
+		[HideInInspector]
+		public float cameraMovementSpeed;
+		[HideInInspector]
+		public float cameraMovementTime;
 
 		private GameObject _targetObject;
 
 		private Vector2 _currentCenter;
 		private Vector2 _targetVector;
+		private Vector2 _previousTargetVector;
+
+		private float _timeSinceOnTarget;
 
 		void Awake()
 		{
@@ -25,29 +36,54 @@
 			CameraZoomer = new CameraZoomer(MainCamera);
 
 			_currentCenter = transform.position;
+
+			_timeSinceOnTarget = 0;
 		}
 
 		void FixedUpdate()
 		{
 			if (_targetObject != null)
 			{
-				_targetVector = _targetObject.transform.position;
-				
+				if (!_targetObject.transform.position.Equals(_targetVector))
+				{
+					_timeSinceOnTarget = 0;
+					_previousTargetVector = _targetVector;
+					_targetVector = _targetObject.transform.position;
+				}
+
 				if (!_currentCenter.Equals(_targetVector))
 				{
-					float distance = cameraSpeed * Time.deltaTime;
-					if (Vector2.Distance(_currentCenter, _targetVector) <= distance)
+					_timeSinceOnTarget += Time.deltaTime;
+					
+					switch (cameraTrackingType)
 					{
-						_currentCenter = _targetVector;
-					}
-					else
-					{
-						Vector2 v = _targetVector - _currentCenter;
-						_currentCenter += distance * v.normalized;
+						case CameraTrackingType.Solid:
+							_currentCenter = _targetVector;
+							break;
+						
+						case CameraTrackingType.Speed:
+							float distance = cameraMovementSpeed * Time.deltaTime;
+							if (Vector2.Distance(_currentCenter, _targetVector) <= distance)
+							{
+								_currentCenter = _targetVector;
+							}
+							else
+							{
+								Vector2 v = _targetVector - _currentCenter;
+								_currentCenter += distance * v.normalized;
+							}
+
+							break;
+						
+						case CameraTrackingType.Time:
+							_currentCenter = Vector2.Lerp(_previousTargetVector, _targetVector,
+								_timeSinceOnTarget / cameraMovementTime);
+							
+							break;
 					}
 				}
 			}
-			
+
 			Vector2 cameraPos = _currentCenter + cameraOffset + CameraShaker.ComputeShake();
 			transform.position = new Vector3(cameraPos.x, cameraPos.y, transform.position.z);
 		}
@@ -71,5 +107,12 @@
 		{
 			_currentCenter = v;
 		}
+	}
+
+	public enum CameraTrackingType
+	{
+		Solid,
+		Speed,
+		Time
 	}
 }
---
+++ Editor/CameraControllerEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CameraControl
+{
+    [CustomEditor(typeof(CameraController))]
+    public class CameraControllerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // Get a reference to the target script
+            CameraController cameraController = (CameraController)target;
+
+            // Draw the default Inspector for the enum
+            DrawDefaultInspector(); 
+
+            // Conditionally draw other fields based on the enum value
+            switch (cameraController.cameraTrackingType)
+            {
+                case CameraTrackingType.Solid:
+                    break;
+                case CameraTrackingType.Speed:
+                    cameraController.cameraMovementSpeed = EditorGUILayout.FloatField("Camera Movement Speed", cameraController.cameraMovementSpeed);
+                    break;
+                case CameraTrackingType.Time:
+                    cameraController.cameraMovementTime = EditorGUILayout.FloatField("Camera Movement Time", cameraController.cameraMovementTime);
+                    break;
+            }
+
+            // Apply changes if any were made in the Inspector
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(cameraController);
+            }
+        }
+    }
+}
